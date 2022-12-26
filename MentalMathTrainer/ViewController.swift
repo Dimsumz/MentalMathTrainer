f5cae48f4f7883f5164d2a2db7218715a9e96740
Jonathan <jonsouyang@gmail.com>
2022-12-26 12:24:48 -0800

    Initial Commit

---
+++ MentalMathTrainer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MentalMathTrainer
+//
+//  Created by Jonathan Ouyang on 12/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
